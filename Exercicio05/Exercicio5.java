36e7ff31f5ce13eebbbfe25ae50132dc08a76f43
JULIANA SEEMANN <juliana.seemann@univille.br>
2026-03-25 20:48:09 -0300

    Exercicio 05

---
+++ Exercicio05/Exercicio5.java
@@ -0,0 +1,12 @@
+// O banco Firmeza desenvolveu um sistema de contas e saldos bancários e lhe contratou para executar um teste de performance para avaliar 
+// a robusteza e a quantidade de transações possíveis de serem processadas por 3 minutos. Para avaliar a performance do sistema foram 
+// criadas 100 contas correntes fictícias com R$ 1.000,00 de saldo inicial. Crie então um teste com programação concorrente que simule 
+// 5 sistemas fazendo transferências de valores aleatórios de até R$ 2.500,00 de uma conta aleatória para outra igualmente aleatória. 
+// A cada 5 segundos, imprima o valor total no cofre do banco, que deverá ser sempre de R$ 100.000,00. Após 3 minutos de execução, 
+// verifique a quantidade de transações que foram executadas e se o valor total no cofre continua sendo R$ 100.000,00.
+
+package Exercicio05;
+
+public class Exercicio5 {
+    
+}
